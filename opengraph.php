bbc8ef33ba57a561a419edccd03ee549d812e445
Parker Moore <parkrmoore@gmail.com>
2013-03-02 18:21:19 +0100

    Always doing the sitename and 'Leacock's

--- opengraph.php
+++ opengraph.php
@@ -117,11 +117,12 @@ function opengraph_default_title( $title ) {
       $author = get_queried_object();
       $title = $author->display_name;
     }
-    if ( opengraph_default_sitename("") ) {
-       $title = $title . " &raquo; " . opengraph_default_sitename("");
-    }
-    $title = $title . " &raquo; Leacock&#039;s";
   }
+  if ( opengraph_default_sitename("") ) {
+     $title = $title . " &raquo; " . opengraph_default_sitename("");
+  }
+  $title = $title . " &raquo; Leacock&#039;s";
+
   return $title;
 }
 
